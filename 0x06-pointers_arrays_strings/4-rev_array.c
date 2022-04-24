a6a67c3427598cfc343a96c140bdac2a0894c75b
Richiealx <richardfrimpongalx@gmail.com>
2022-04-24 08:19:12 -0700

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse array of integers
+ * @a: array
+ * @n: number of elements of array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int j;
+	int temp;
+
+	for (j = 0; j < n--; j++)
+	{
+		temp = a[j];
+		a[j] = a[n];
+		a[n] = temp;
+	}
+}
