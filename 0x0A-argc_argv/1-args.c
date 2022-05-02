16219a122c312e49ecb7c25b306e88beeb423443
Richiealx <richardfrimpongalx@gmail.com>
2022-05-02 11:46:25 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+/*
+ * File: 0-whatsmyname.c
+ * Auth: Richard Frimpong
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the number of arguments passed into thre function
+ * @argc: The argument count
+ * @argv: The argument vector
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
