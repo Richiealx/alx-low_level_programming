c68f8e37fe1a1894ed3e90c130c5124dd4978b1a
Richiealx <richardfrimpongalx@gmail.com>
2022-05-09 07:45:30 -0700

    Task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,27 +1,29 @@
-​#​define​ ​_HEADER_ 
+​fndef​ _HEADER_ 
+ ​#​define​ ​_HEADER_ 
+   
+  ​/*​* 
+	   ​  * struct dog - Dog attributes 
+	    ​  * @name: The name of the dog. 
+	     ​  * @age: The age of the dog. 
+	      ​  * @owner: The owner of the dog. 
+	       ​  * 
+	        ​  * Description: The attributes of a dog. 
+		 ​  ​*/ 
+   ​struct​ dog 
+    ​{ 
+	     ​        ​char​ *name; 
+	      ​        ​float​ age; 
+	       ​        ​char​ *owner; 
+	        ​}; 
   
  ​/*​* 
- ​  * struct dog - Dog attributes 
- ​  * @name: The name of the dog. 
- ​  * @age: The age of the dog. 
- ​  * @owner: The owner of the dog. 
- ​  * 
- ​  * Description: The attributes of a dog. 
- ​  ​*/ 
- ​struct​ dog 
- ​{ 
- ​        ​char​ *name; 
- ​        ​float​ age; 
- ​        ​char​ *owner; 
- ​}; 
-  
- ​/*​* 
- ​ * dog_t - Typedef for dog structure 
- ​ ​*/ 
- ​typedef​ ​struct​ dog ​dog_t​; 
-  
- ​void​ ​init_dog​(​struct​ dog *d, ​char​ *name, ​float​ age, ​char​ *owner); 
- ​void​ ​print_dog​(​struct​ dog *d); 
- ​dog_t​ *​new_dog​(​char​ *name, ​float​ age, ​char​ *owner); 
- ​void​ ​free_dog​(​dog_t​ *d); 
+	  ​ * dog_t - Typedef for dog structure 
+	   ​ ​*/ 
+  ​typedef​ ​struct​ dog ​dog_t​; 
+    
+   ​void​ ​init_dog​(​struct​ dog *d, ​char​ *name, ​float​ age, ​char​ *owner); 
+    ​void​ ​print_dog​(​struct​ dog *d); 
+     ​dog_t​ *​new_dog​(​char​ *name, ​float​ age, ​char​ *owner); 
+      ​void​ ​free_dog​(​dog_t​ *d); 
+       ​#​endifvoid​ ​free_dog​(​dog_t​ *d); 
  ​#​endif​#​endif
